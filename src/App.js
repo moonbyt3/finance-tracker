3a8b303cba85ea25271230b41ec1348fd859a4e1
moonbyt3 <moonbyt3@gmail.com>
2020-02-04 01:27:46 +0100

    Initial routing, basic scss architecture

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from './components/About/About';
+import Tracker from './components/Tracker/Tracker';
+
+import './scss/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+	return (
+		<div className="container-fluid">
+		<Router>
+			<div className="row">
+				<nav className="navigation">
+					<Link to="/" className="navigation__link">Home</Link>
+					<Link to="/about" className="navigation__link">About</Link>
+				</nav>
+			</div>
+		<Switch>
+			<Route exact path="/" component={Tracker}></Route>
+			<Route path="/about" component={About}></Route>
+		</Switch>
+		</Router>
+		</div>
+		);
+	}
+	
+	export default App;
+	
---
+++ src/components/Tracker/Tracker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './tracker.scss';
+
+export default function Tracker() {
+    return (
+        <div className="container">
+            <h4>Your Balance</h4>
+            <h1 id="balance">$32.00</h1>
+
+            
+        </div>
+    )
+}
