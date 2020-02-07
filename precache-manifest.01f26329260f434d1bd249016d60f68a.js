3fb098acffa7391bfb0bbfcf237cfd36995c7f74
moonbyt3 <moonbyt3@gmail.com>
2020-02-07 23:51:14 +0100

    Updates

--- precache-manifest.01f26329260f434d1bd249016d60f68a.js
+++ precache-manifest.01f26329260f434d1bd249016d60f68a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d050222c997ce0261ce644c6d3e7bb68",
+    "revision": "16cf5e4916511fb407bd3860c8ad1b0a",
     "url": "/finance-tracker/index.html"
   },
   {
-    "revision": "4ff4110212d5fa14b4d3",
+    "revision": "e3baa7219d1c8e12835c",
     "url": "/finance-tracker/static/css/main.57c47c62.chunk.css"
   },
   {
-    "revision": "25af8cdaec3d6b19cc95",
-    "url": "/finance-tracker/static/js/2.6926162b.chunk.js"
+    "revision": "6da273524fb0148917f3",
+    "url": "/finance-tracker/static/js/2.46d414c1.chunk.js"
   },
   {
     "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
-    "url": "/finance-tracker/static/js/2.6926162b.chunk.js.LICENSE.txt"
+    "url": "/finance-tracker/static/js/2.46d414c1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4ff4110212d5fa14b4d3",
-    "url": "/finance-tracker/static/js/main.4023be51.chunk.js"
+    "revision": "e3baa7219d1c8e12835c",
+    "url": "/finance-tracker/static/js/main.77f47b9e.chunk.js"
   },
   {
     "revision": "8f5ea0f070dd480232c3",
